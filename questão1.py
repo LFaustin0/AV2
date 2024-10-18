967e2204860c1ea638333ecd97e760edc49c5949
LFaustin0 <lucasfaustinogsilva2007@gmail.com>
2024-10-18 01:45:31 -0300

    questão1.py

---
+++ questão1.py
@@ -0,0 +1,9 @@
+def polaridade(numero):
+    if numero > 0:
+        print('Positivo')
+    elif numero <= 0:
+        print('Negativo')
+
+  
+n1 = int(input("Digite um número: "))
+polaridade (num)
