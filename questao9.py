c03d586269653f7f56446f4f34ee598d0755ff7d
LFaustin0 <lucasfaustinogsilva2007@gmail.com>
2024-10-18 02:07:06 -0300

    questao9.py

---
+++ questao9.py
@@ -0,0 +1,10 @@
+import contagem
+
+def palavras (string):
+    
+    total = conta.findall('[ ]', string)
+
+    print(f"Total de palavras: {len(total) + 1}")
+
+string = 'essa frase contém 10 palavras'
+palavras(string)
